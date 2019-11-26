d7d3c6a5316674803301bf00a3614f150064af7e
PeiYumeng <457492875@qq.com>
2019-11-26 16:57:16 +0800

    message

--- chuanda_jiekou/location.js
+++ chuanda_jiekou/location.js
@@ -27,13 +27,12 @@ con.query('select * from citys', (err, result) => {
     }
     //获取某市天气
     console.log(city)
-    // var addr = 'http://v.juhe.cn/weather/index?cityname=' + city + '&key=70b20823f67b5f0ca3358b796fd83260';
+    var addr = 'http://v.juhe.cn/weather/index?cityname=' + city + '&key=70b20823f67b5f0ca3358b796fd83260';
     // http.get(global.encodeURI(addr), (res) => {
     //     res.on('data', (data) => {
     //     result += data.toString('utf8');
     //     });
     // });
-    fetch(addr).then(res=>res.json).then(res=>console.log(res))
     // console.log(result);
   });
  
