a1619ff95ff75cb8467d18dbfdcd2c242182948c
PeiYumeng <457492875@qq.com>
2019-11-26 15:24:53 +0800

    message

--- chuanda_jiekou/location.js
+++ chuanda_jiekou/location.js
@@ -1,16 +1,37 @@
 #!/usr/bin/node
 
 const http = require('http');
-
-var city = process.argv[2] || '石家庄';  //设置默认城市石家庄
-var addr = 'http://v.juhe.cn/weather/index?cityname=' + city + '&key=70b20823f67b5f0ca3358b796fd83260';
+//连接数据库
+const mysql = require('mysql'),
+      con   = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'ddd',
+        database: 'test'
+      });
+con.connect();
+//设置城市
+var city,result='';
+//查询数据库数据
+con.query('select * from citys', (err, result) => {
+    if(err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+    console.log(result[0].title);
+    if(result[0].title==='chengdu') city='成都'
+    else city='石家庄'
+  });
+  con.end();
 //获取某市天气
-var result='';
+var addr = 'http://v.juhe.cn/weather/index?cityname=' + city + '&key=70b20823f67b5f0ca3358b796fd83260';
 http.get(global.encodeURI(addr), (res) => {
     res.on('data', (data) => {
       result += data.toString('utf8');
     });
 });
+
+//创建服务
 http.createServer((req,res)=>{
     if(req.url==='/weather'){
         res.setHeader("Access-Control-Allow-Origin", "*"); 
