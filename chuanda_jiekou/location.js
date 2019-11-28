aa8baa6814fb857e09aa2129039e79a69c1e444a
PeiYumeng <457492875@qq.com>
2019-11-28 21:18:10 +0800

    message

--- chuanda_jiekou/location.js
+++ chuanda_jiekou/location.js
@@ -4,7 +4,7 @@ const http = require('http');
 //连接数据库
 const mysql = require('mysql'),
       con   = mysql.createConnection({
-        host: 'http://zhangpeiligao.top',
+        host: 'http://47.98.163.228',
         user: 'root',
         password: 'ddd',
         database: 'clothes'
